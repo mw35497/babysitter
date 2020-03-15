1dc18c7152d505c940247cd5c3986ddc24f50cab
Matthew Wade <matthewgeowade@utexas.edu>
2020-03-15 15:48:45 -0500

    relativeTimeTest_Fail

--- test_babysitter.py
+++ test_babysitter.py
@@ -1,42 +1,41 @@
 import unittest
 import babysitter
 
-case_1 = ['4:59pm','6:59pm','A']
-case_2 = ['5:00pm','2:37am','A']
-case_3 = ['12:59am','12:09pm','B']
-case_4 = ['12:33pm','12:00am','B']
-case_5 = ['4:18am','4:00pm','C']
-case_6 = ['6:4pm','3:002pm','C']
-case_7 = ['7:63am','3:76pm','D']
-case_8 = ['7:22em','1:59om','1']
-case_9 = ['6:29pm','6:28pm','A']
-case_10 = ['8:38pm','7:26pm','A']
-
 class TestBabysitter(unittest.TestCase):
 
 	def test_timeTest(self):
-		self.assertEqual(babysitter.timeTest(case_1[0]), False)
-		self.assertEqual(babysitter.timeTest(case_2[0]), True)
-		self.assertEqual(babysitter.timeTest(case_3[0]), True)
-		self.assertEqual(babysitter.timeTest(case_4[0]), False)
-		self.assertEqual(babysitter.timeTest(case_5[0]), False)
-		self.assertEqual(babysitter.timeTest(case_6[0]), False)
-		self.assertEqual(babysitter.timeTest(case_7[0]), False)
-		self.assertEqual(babysitter.timeTest(case_8[0]), False)
-		self.assertEqual(babysitter.timeTest(case_9[0]), True)
-		self.assertEqual(babysitter.timeTest(case_10[0]), True)
-		self.assertEqual(babysitter.timeTest(case_1[1]), True)
-		self.assertEqual(babysitter.timeTest(case_2[1]), True)
-		self.assertEqual(babysitter.timeTest(case_3[1]), False)
-		self.assertEqual(babysitter.timeTest(case_4[1]), True)
-		self.assertEqual(babysitter.timeTest(case_5[1]), False)
-		self.assertEqual(babysitter.timeTest(case_6[1]), False)
-		self.assertEqual(babysitter.timeTest(case_7[1]), False)
-		self.assertEqual(babysitter.timeTest(case_8[1]), False)
-		self.assertEqual(babysitter.timeTest(case_9[1]), True)
-		self.assertEqual(babysitter.timeTest(case_10[1]), True)
+		self.assertEqual(babysitter.timeTest('4:59pm'), False)
+		self.assertEqual(babysitter.timeTest('5:00pm'), True)
+		self.assertEqual(babysitter.timeTest('12:59am'), True)
+		self.assertEqual(babysitter.timeTest('12:33pm'), False)
+		self.assertEqual(babysitter.timeTest('4:18am'), False)
+		self.assertEqual(babysitter.timeTest('6:4pm',), False)
+		self.assertEqual(babysitter.timeTest('7:63am'), False)
+		self.assertEqual(babysitter.timeTest('7:22em'), False)
+		self.assertEqual(babysitter.timeTest('6:29pm'), True)
+		self.assertEqual(babysitter.timeTest('8:38pm'), True)
+		self.assertEqual(babysitter.timeTest('6:59pm'), True)
+		self.assertEqual(babysitter.timeTest('2:37am'), True)
+		self.assertEqual(babysitter.timeTest('12:09pm'), False)
+		self.assertEqual(babysitter.timeTest('12:00am'), True)
+		self.assertEqual(babysitter.timeTest('4:00pm'), False)
+		self.assertEqual(babysitter.timeTest('3:002pm'), False)
+		self.assertEqual(babysitter.timeTest('3:76pm'), False)
+		self.assertEqual(babysitter.timeTest('1:59om'), False)
+		self.assertEqual(babysitter.timeTest('6:28pm'), True)
+		self.assertEqual(babysitter.timeTest('7:26pm'), True)
 
-
+	def test_relativeTimeTest(self):
+		self.assertEqual(babysitter.relativetimeTest('5:00pm','4:00am'), True)
+		self.assertEqual(babysitter.relativetimeTest('4:00am','5:00pm'), False)
+		self.assertEqual(babysitter.relativetimeTest('12:00am','3:33pm'), True)
+		self.assertEqual(babysitter.relativetimeTest('11:02pm','12:51am'), True)
+		self.assertEqual(babysitter.relativetimeTest('8:09pm','7:26pm'), False)
+		self.assertEqual(babysitter.relativetimeTest('5:04pm','5:03pm'), False)
+		self.assertEqual(babysitter.relativetimeTest('6:03pm','7:03pm'), True)
+		self.assertEqual(babysitter.relativetimeTest('2:46am','12:42am'), False)
+		self.assertEqual(babysitter.relativetimeTest('1:23am','2:22am'), True)
+		self.assertEqual(babysitter.relativetimeTest('2:22pm','5:10pm'), False)
 
 if __name__ == '__main__':
 	unittest.main()
